5f966ceb8bf1f3c4d0a0b5fc21f4a3054c12e548
Marijn <marijnstam@live.nl>
2020-01-12 14:54:07 +0100

    Gripper position is now tracked

--- arm_interface/src/armMain.cpp
+++ arm_interface/src/armMain.cpp
@@ -45,17 +45,21 @@
 #include <moveit/robot_state/robot_state.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf/transform_datatypes.h>
-
+#include <moveit_msgs/PlanningScene.h>
 
 
 
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "move_group_interface_tutorial");
-  ros::NodeHandle node_handle;
+  ros::init(argc, argv, "arm_interface");
+  ros::NodeHandle node_handle("~");
   ros::AsyncSpinner spinner(1);
   spinner.start();
+  ROS_INFO("Waiting for context to come up");
+  sleep(10);
+  ROS_INFO("Starting interface...");
+
 
   // BEGIN_TUTORIAL
   //
@@ -65,18 +69,7 @@ int main(int argc, char** argv)
   // MoveIt! operates on sets of joints called "planning groups" and stores them in an object called
   // the `JointModelGroup`. Throughout MoveIt! the terms "planning group" and "joint model group"
   // are used interchangably.
-  static const std::string PLANNING_GROUP = "arm";
 
-  // The :move_group_interface:`MoveGroup` class can be easily
-  // setup using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
-  // We will use the :planning_scene_interface:`PlanningSceneInterface`
-  // class to add and remove collision objects in our "virtual world" scene
-
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const robot_state::JointModelGroup* joint_model_group =
-     move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
 
 
@@ -96,7 +89,24 @@ int main(int argc, char** argv)
   // ^^^^^^^^^^^^^^^^^^^^^^^^^
   //
   // We can print the name of the reference frame for this robot.
-  joint_model_group->printGroupInfo();
+  
+
+  static const std::string PLANNING_GROUP = "arm";
+
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+  robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
+/* Create a RobotState and JointModelGroup to keep track of the current robot pose and planning group*/
+  robot_state::RobotStatePtr robot_state(new robot_state::RobotState(robot_model));
+  const robot_state::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  std::cout << "EEF Name: " << joint_model_group->getEndEffectorName() << std::endl;
+
+  // const robot_state::JointModelGroup* joint_model_group =
+  //    move_group.getCurrentState()->getJointModelGroup("arm");
+
+  // joint_model_group->printGroupInfo();
   move_group.setNumPlanningAttempts(10);
 
 
@@ -104,6 +114,8 @@ int main(int argc, char** argv)
 
   // We can also print the name of the end-effector link for this group.
   ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+  // ROS_INFO_NAMED("tutorial", "End effector link: %s", joint_model_group->getEndEffectorName().c_str());
+
 
   geometry_msgs::PoseStamped robot_pose;
   robot_pose = move_group.getCurrentPose();
@@ -114,22 +126,28 @@ int main(int argc, char** argv)
   current_position = robot_pose.pose;
 
   /*Retrive position and orientation */
-  geometry_msgs::Point exact_pose = current_position.position;
-  geometry_msgs::Quaternion exact_orientation = current_position.orientation;
+  geometry_msgs::Point exact_pose; 
+  geometry_msgs::Quaternion exact_orientation; 
+  exact_pose = current_position.position;
+  exact_orientation = current_position.orientation;
 
-  std::cout<<"Reference frame: "<<robot_pose.header.frame_id<<std::endl;
-  std::cout<<"Robot position : "<<exact_pose.x<<"\t"<<exact_pose.y<<"\t"<<exact_pose.z<<std::endl;
-  std::cout<<"Robot Orientation : "<<exact_orientation.x<<"\t"<<exact_orientation.y<<"\t"<<exact_orientation.z<<"\t"<<exact_orientation.w<<std::endl;
-
+  // while(1){
+  //   robot_pose = move_group.getCurrentPose();
+  //   current_position = robot_pose.pose;
+  //   exact_pose = current_position.position;
+  //   exact_orientation = current_position.orientation;
+    
+  //   std::cout<<"Reference frame: "<<robot_pose.header.frame_id<<std::endl;
+  //   ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
+  //   std::cout<<"Robot position : "<<exact_pose.x<<"\t"<<exact_pose.y<<"\t"<<exact_pose.z<<std::endl;
+  //   std::cout<<"Robot Orientation : "<<exact_orientation.x<<"\t"<<exact_orientation.y<<"\t"<<exact_orientation.z<<"\t"<<exact_orientation.w<< "\n" << std::endl;
+  //   sleep(3);
+  // }
   // Start the demo
   // ^^^^^^^^^^^^^^^^^^^^^^^^^
 
+  move_group.getPlanningFrame();
   moveit::planning_interface::MoveGroupInterface::Plan plan;
-  
-  // Planning to a Pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^
-  // We can plan a motion for this group to a desired pose for the
-  // end-effector.
 
   geometry_msgs::Pose target_pose1;
 
@@ -137,11 +155,11 @@ int main(int argc, char** argv)
   target_pose1.position.y = 0.0897;
   target_pose1.position.z = 0.3766;
   target_pose1.orientation.w = 1;
-  target_pose1.orientation.x = 0;
-  target_pose1.orientation.y = 0;
-  target_pose1.orientation.z = 0;
+  // target_pose1.orientation.x = exact_orientation.x;
+  // target_pose1.orientation.y = exact_orientation.y;
+  // target_pose1.orientation.z = exact_orientation.z;
 
-  move_group.setPoseTarget(target_pose1, move_group.getEndEffectorLink().c_str());
+  move_group.setPoseTarget(target_pose1);
 
 
   bool success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
@@ -155,8 +173,8 @@ int main(int argc, char** argv)
   // bool success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
   // ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  sleep(5);
-  move_group.execute(plan);
+  // sleep(5);
+  // move_group.execute(plan);
 
   return 0;
 }
